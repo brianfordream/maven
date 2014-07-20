f418a14a6b24532e78d5887c9bb3f560ea6ca011
brianfordram <yang199095@163.com>
2014-07-20 20:07:10 +0800

    log

---
+++ user-log/src/main/java/com/btgxlm/study/user_log/App.java
@@ -0,0 +1,16 @@
+package com.btgxlm.study.user_log;
+
+import com.btgxlm.core.model.User;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        User user=new User();
+    }
+}
