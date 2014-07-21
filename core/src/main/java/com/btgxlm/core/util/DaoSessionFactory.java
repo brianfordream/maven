7292c0fe17c4e5a3d930e65b6d99b98971e2fe13
brianfordram <yang199095@163.com>
2014-07-21 18:36:31 +0800

    c

--- core/src/main/java/com/btgxlm/core/util/DaoSessionFactory.java
+++ core/src/main/java/com/btgxlm/core/util/DaoSessionFactory.java
@@ -8,6 +8,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 import org.springframework.stereotype.Service;
-@Service
+
 public class DaoSessionFactory extends HibernateDaoSupport {	
 }
