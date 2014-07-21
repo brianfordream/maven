ffb089b50413e7a8270d4e973493b6209c2bbbc3
brianfordram <yang199095@163.com>
2014-07-21 18:03:21 +0800

    a

--- core/src/main/java/com/btgxlm/core/util/DaoSessionFactory.java
+++ core/src/main/java/com/btgxlm/core/util/DaoSessionFactory.java
@@ -4,6 +4,8 @@ import javax.annotation.Resource;
 
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DaoSessionFactory {
 	private SessionFactory sessionFactory;
@@ -11,6 +13,8 @@ public class DaoSessionFactory {
 		return getSessionFactory();
 	}
 	public SessionFactory getSessionFactory() {
+		ApplicationContext context=new ClassPathXmlApplicationContext();
+		SessionFactory sessionFactory = (SessionFactory) context.getBean("mySessionFactory");
 		return sessionFactory;
 	}
 	public void setSessionFactory(SessionFactory sessionFactory) {
