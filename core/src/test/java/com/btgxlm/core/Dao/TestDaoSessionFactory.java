72a92c8cb8206d411d81dc09b908bae993623e83
brianfordram <yang199095@163.com>
2014-07-21 18:33:40 +0800

    fs

--- core/src/test/java/com/btgxlm/core/Dao/TestDaoSessionFactory.java
+++ core/src/test/java/com/btgxlm/core/Dao/TestDaoSessionFactory.java
@@ -5,12 +5,15 @@ import junit.framework.Assert;
 
 import org.hibernate.SessionFactory;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.btgxlm.core.util.DaoSessionFactory;
 
 public class TestDaoSessionFactory {
 	@Test
 	public void test(){
+		ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
 		SessionFactory sessionFactory=new DaoSessionFactory().getSessionFactory();
 		assertNotNull(sessionFactory);
 	}
