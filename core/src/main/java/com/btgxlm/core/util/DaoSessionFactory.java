2271446651443d236bbdb43718461b49f0265b0f
brianfordram <yang199095@163.com>
2014-07-21 17:15:48 +0800

    afa

--- core/src/main/java/com/btgxlm/core/util/DaoSessionFactory.java
+++ core/src/main/java/com/btgxlm/core/util/DaoSessionFactory.java
@@ -1,5 +1,12 @@
 package com.btgxlm.core.util;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class DaoSessionFactory {
+	@Autowired
 	private SessionFactory sessionFactory;
+	public SessionFactory GetSessionFactory(){
+		return sessionFactory;
+	}
 }
---
+++ core/src/test/java/com/btgxlm/core/Dao/TestDaoSessionFactory.java
@@ -0,0 +1,17 @@
+package com.btgxlm.core.Dao;
+
+import static org.junit.Assert.assertNotNull;
+import junit.framework.Assert;
+
+import org.hibernate.SessionFactory;
+import org.junit.Test;
+
+import com.btgxlm.core.util.DaoSessionFactory;
+
+public class TestDaoSessionFactory {
+	@Test
+	public void test(){
+		SessionFactory sessionFactory=new DaoSessionFactory().GetSessionFactory();
+		assertNotNull(sessionFactory);
+	}
+}
